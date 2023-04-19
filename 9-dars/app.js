9fc3e6f216180924f6883f120e1476ef6de5a5c7
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2023-04-19 06:16:38 +0500

    String

---
+++ 9-dars/app.js
@@ -0,0 +1,30 @@
+// String
+// ( \ )   backslash o'zidan keyingi character ni ignore qiladi
+
+// String 2 xil  usulda yasaladi, jumladan variable orqali hamda string constructor orqali
+
+let strOne = "Mirzo Ulug'bek" // string qaytaradi
+let strTwo = new String("Mirzo Ulug'bek"); // object qaytaradi
+
+// Stringlarni taqqoslash
+// localCompare()
+let str1 = "Webbrain"
+let str2 = "Webrain"
+
+console.log(str1.localeCompare(str2)) // -1[qiymat yo'q], 0[exactly bir-biriga mos], 1[qiymat qisman bor]
+
+
+// Mutable and inmutable
+// string inmutable hisoblanadi ya'ni ichidagi qiymatni indexi bo'yicha o'zgartira olmaymiz
+// Qoida: index manfiy qiymat qabul qilmaydi
+
+let name = "Beginur"
+name[0] = "D" // ❌ inmutable
+console.log(name); // Beginur
+
+// by index[] = by .at = by charAt(index)
+
+// Accesing characters 
+// by index[] - only positivenumbers
+// by .at(index) - can get minus value  (-1 orqadan boshlab hisoblaydi)
+// by .charAt(index) - not minus value
--- homeworks/app.js
+++ homeworks/app.js
@@ -12,11 +12,11 @@ console.log(user); // log
 let schedule = {};
 
 function isEmpty() {
-if (schedule.length === 0) {
-  console.log(true);
-} else {
-  console.log(false);
-}
+  if (schedule.length === 0) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
 }
 
 alert(isEmpty(schedule)); //true
@@ -24,8 +24,31 @@ schedule["8:30"] = "get up";
 alert(isEmpty(schedule)); // false
 
 // 3
-// Istalgan sonni 2 link sanoq sitemasiga o'tkazib, nechta 0 vanechta bir qatnashganini aniqlang
+// Istalgan sonni 2 link sanoq sitemasiga o'tkazib, nechta 0 va nechta 1 qatnashganini aniqlang.
+let decimalNumber = +prompt();
+let value = decimalNumber.toString(2);
+let numOfZeros = 0;
+let numOfOnes = 0;
+for (let i = 0; i < value.length; i++) {
+  if (value[i] === "0") {
+    numOfZeros++;
+  } else if (value[i] === "1") {
+    numOfOnes++;
+  }
+}
+
+console.log(`${decimalNumber} soni 2 lik sanoq sistemasida yozilsa`)
+console.log(`${numOfZeros}ta 0 qatnashadi`);
+console.log(`${numOfOnes}ta 1 qatnashadi`);
+
+console.log(`Asl holatida esa: ${value}`)
+
 
 // Berilgan sonlar ichida eng katta va eng kichik sonlar ayirmasini toping. Musbat son chiqmasin
 
+
+
 // 1-50 gacha bo'lgan sonlardan random aniqlab oladigan function yozish
+
+// Find randomNumbers
+
