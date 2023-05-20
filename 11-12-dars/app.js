df5a50106a16b274c70e0d7d456f18dd43676abc
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2023-05-20 08:51:46 +0500

    new Map()

--- 11-12-dars/app.js
+++ 11-12-dars/app.js
@@ -3,34 +3,71 @@
 // Callback function oladigan barcha elementlar xuddi for loopga o'xshab aylanib chiqadi.
 
 // C - Create
+// use spread ( ... ), distructure
+
 // R - Reader
+// filter, sort, includes, find
+
 // U - Update
+// filter, find
+
 // D - Delete
+// filter, splice
 
 
-// const students = [
-//   {sex: "male",id: 1, name: "Abdurashid", age: 2004, major: "student"},
-//   {sex: "male",id: 7, name: "Ubaydullo", age: 2007, major: "student"},
-//   {sex: "male",id: 2, name: "Asilbek", age: 2002, major: "student"},
-//   {sex: "female",id: 8, name: "Gavharoy", age: 2003, major: "teacher"},
-//   {sex: "female",id: 14, name: "Qovunoy", age: 2003, major: "teacher"},
-//   {sex: "female",id: 12, name: "Mohlaroy", age: 2007, major: "teacher"},
-//   {sex: "female",id: 3, name: "Beginur", age: 2004, major: "teacher"},
-//   {sex: "female",id: 10, name: "Shaxzoda", age: 2000, major: "teacher"},
-//   {sex: "male",id: 4, name: "Muhammadyusuf", age: 2006, major: "student"},
-//   {sex: "female",id: 5, name: "Muslima", age: 2006, major: "student"},
-//   {sex: "female",id: 9, name: "Umida", age: 2005, major: "student"},
-//   {sex: "male",id: 6, name: "Mirzo Ulug'bek", age: 2000, major: "teacher"},
-//   {sex: "female",id: 13, name: "Shaxzoda", age: 2004, major: "student"},
-//   {sex: "female",id: 11, name: "Nafratxon", age: 2002, major: "student"},
-// ]
+let students = [
+  {sex: "male",id: 1, name: "Urinbayev Abdurashid", age: 2004, major: "student"},
+  {sex: "male",id: 7, name: "Qosimov Ubaydullo", age: 2007, major: "student"},
+  {sex: "male",id: 2, name: "Solijonov Asilbek", age: 2002, major: "student"},
+  {sex: "female",id: 8, name: "Yulchiyeva Gavharoy", age: 2003, major: "teacher"},
+  {sex: "female",id: 14, name: "Torvuzova Qovunoy", age: 2003, major: "teacher"},
+  {sex: "female",id: 12, name: "Egamberidiyeva Mohlaroy", age: 2007, major: "teacher"},
+  {sex: "female",id: 3, name: "Begijonova Beginur", age: 2004, major: "teacher"},
+  {sex: "female",id: 10, name: "Ulkanboyeva Shaxzoda", age: 2005, major: "teacher"},
+  {sex: "male",id: 4, name: "Mirzayev Muhammadyusuf", age: 2006, major: "student"},
+  {sex: "female",id: 5, name: "Mirzayeva Muslima", age: 2006, major: "student"},
+  {sex: "female",id: 9, name: "Xomidova Umida", age: 2005, major: "student"},
+  {sex: "male",id: 6, name: "Xudoyberdiyev Mirzo Ulug'bek", age: 2000, major: "teacher"},
+  {sex: "female",id: 13, name: "Tomchiyeva Shaxzoda", age: 2004, major: "student"},
+  {sex: "female",id: 11, name: "Nafrataliyeva Nafratxon", age: 2002, major: "student"},
+]
+
+let map = new Map([["name", "Mirzo Ulug'bek"], ["id", 456], ["major", "futureTeacher"]])
+// console.log(map.entries()); // key and value
+// console.log(map.values()); // only values
+// console.log(map.keys()); // only keys
+
+// let gen = map.entries();      ***********
+// gen.next();                             *
+// gen.next();                             ****** ==> "ruchnoy loop 😉"
+// gen.next();                   ***********
+// console.log(gen.next()); ******
 
 
 
-// let sortedStudents = students.sort((a, b) => a.age - b.age)
+// const updateUserName = (obj) => {
+//   let result = students.map(student => student.id === obj.id ? {...student, [obj.type]: obj.value} : student);
+//   students = result;
+// };
 
-// console.log(sortedStudents);
+// updateUserName({id: 11, type: "major", value: "teacher"})
+// console.log(students);
 
+// const sortByAlphabet = students.sort((a, b) => a.name.localeCompare(b.name))
+
+// const sortByYear = students.sort((a, b) => a.age - b.age);
+
+// const deleteUserById = (id) => {
+//   let res = students.filter(value => value.id !== id)
+//   students = res
+//   console.log(students);
+// };
+
+// deleteUserById(3)
+
+// let mapStudents = students.map(value => value.id === 3 ? value : null)
+
+// console.log(mapStudents);
 
 
 
@@ -108,18 +145,18 @@
 // 3-parametr indentation bu number qabul qiladi, nima qiladi deydigan bo'lsak objectlar nested bo'lganda ko'rishimizga oson qilib bergan number imiz qiymaticha bo'sh joy(space) qo'yib beradi. Bu umuman olganda formatting ya'ni readableity ni oshirishga hizmat qiladi, o'qishimiz uchun qulay ko'rinishga olib keladi
 
 
-let obj = {
-  name: "Mirzo Ulug'bek",
-  id: 4567,
-  major: "hacker",
-  data: {
-    fullnName: "Xudoyberdiyev Mirzo Ulug'bek",
-    birthDate: 02022004
-  }
-}
+// let obj = {
+//   name: "Mirzo Ulug'bek",
+//   id: 4567,
+//   major: "hacker",
+//   data: {
+//     fullnName: "Xudoyberdiyev Mirzo Ulug'bek",
+//     birthDate: 02022004
+//   }
+// }
 
 
-let toJSON = JSON.stringify(obj)
-let toObject = JSON.parse(toJSON)
+// let toJSON = JSON.stringify(obj)
+// let toObject = JSON.parse(toJSON)
 
 // JSON.parse(value) ==> JSONni objectga o'giradi parametr sifatida value ya'ni JSONni qabul qiladi...
